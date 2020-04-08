4a37af0a1d4e9fb696ad5813687131f57718ab4b
ksamrish <61387380+ksamrish@users.noreply.github.com>
2020-04-08 23:31:15 +0530

    Update day7_remove_duplicates.py

--- day7_remove_duplicates.py
+++ day7_remove_duplicates.py
@@ -4,3 +4,18 @@ for i in range(len(inp)):
     if(l.count(inp[i])<1):
         l.append(inp[i])
 print(*l,sep='')
+
+
+
+//another method::
+inp = input()
+l = []
+l.append(inp[0])
+for i in range(len(inp)):
+    for j in range(i):
+        if(inp[i]==inp[j]):
+            break
+        if(j==i-1):
+            l.append(inp[i])
+
+print(*l,sep='')
